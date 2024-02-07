e1645858a06362017718f3852d38badc002ce1e2
lucasporrini <2608lucas@gmail.com>
2024-02-07 21:39:41 +0100

    ➕ Added: background into header

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -86,7 +86,7 @@ $(function() {
       transition: 'fade2',
       transitionDuration: 2000,
       slides: [
-        { src: "https://dummyimage.com/960x1080/4d4d4d/636363" },
+        { src: "./public/assets/img/backgrounds/header.webp" },
         { src: "https://dummyimage.com/960x1080/4d4d4d/636363" },
         { src: "https://dummyimage.com/960x1080/4d4d4d/636363" }
       ],
